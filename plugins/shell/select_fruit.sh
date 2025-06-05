6dd028c2140add0c8c5334152fa52e58c4470bda
goldcollar95 <yjs7298@naver.com>
2025-06-05 14:06:07 +0900

    plugins 폴더추가

--- plugins/shell/select_fruit.sh
+++ plugins/shell/select_fruit.sh
@@ -1,10 +1,10 @@
 FRUIT=$1
 if [ $FRUIT == APPLE ];then
-	echo "You selected Apple!"
+	echo "You selected Apple1!"
 elif [ $FRUIT == ORANGE ];then
-	echo "You selected Orange!"
+	echo "You selected Orange1!"
 elif [ $FRUIT == GRAPE ];then
-	echo "You selected Grape!"
+	echo "You selected Grape!1"
 else
 	echo "Yout selected other Fruit!"
 fi
